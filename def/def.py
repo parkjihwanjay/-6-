f6bf38952050847eb688cc432a2ade5292f9a37b
ParkJiHwan <cc6656@naver.com>
2020-04-16 21:07:35 +0900

    함수 추가

---
+++ def/def.py
@@ -0,0 +1,34 @@
+def add(a, b):
+    return a+b
+
+
+result = add(a, b)
+print(result)
+
+
+def show_add(a, b):
+    print(a+b)
+
+
+def get_number():
+    return 4
+
+
+def say_hello():
+    print('hello')
+
+
+say_hello()
+
+
+def add_and_mul(a, b):
+    return a+b
+    return a*b
+
+
+def add_and_mul(a, b):
+    return a+b, a*b
+
+
+result = add_and_mul(2, 3)
+print(result)
